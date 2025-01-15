0408a56c7e13e60c49179d41d965cb83107ab972
Mathmatlol <mathias.degourmont@free.fr>
2025-01-15 22:13:52 +0100

    nouvelle version de traitement

--- TP/traitement.php
+++ TP/traitement.php
@@ -1,3 +1,13 @@
 <?php
-    echo("ok");
+	if((isset($_GET["email"]) && isset($_GET["name"]))){
+		if((!empty($_GET["email"])) && (!empty($_GET["email"]))){
+			$verifsmail = htmlspecialchars($_GET["email"], ENT_QUOTES);
+			$verifsname = htmlspecialchars($_GET["name"], ENT_QUOTES);
+			echo $verifsmail;
+			echo "</br>";
+			echo $verifsname;
+			echo "</br>";
+			echo("Tout est ok");
+		}
+	}
 ?>
